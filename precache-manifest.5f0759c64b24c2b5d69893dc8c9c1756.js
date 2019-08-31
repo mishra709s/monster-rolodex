bf3b098e7f963617844a43a12253c8ec61647eba
Soubhagya Mishra <trippywords0101@gmail.com>
2019-08-31 14:27:45 +0530

    Updates

---
+++ precache-manifest.5f0759c64b24c2b5d69893dc8c9c1756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe5870aefe8b14d8199e4e3ec35b11e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4820da465c79c723861c",
+    "url": "/monster-rolodex/static/css/main.1505bc8b.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monster-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "4820da465c79c723861c",
+    "url": "/monster-rolodex/static/js/main.ab43f4d1.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
