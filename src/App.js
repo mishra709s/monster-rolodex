30aaa21d5abd3aa4ba81e7d3174cd99193975d35
soubhagya-addvector <soubhagya@addvector.com>
2019-12-31 14:00:14 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -23,9 +23,9 @@ class App extends Component {
       .then(data => this.setState({ monsters: data }));
   }
   render() {
-    const { monsters, searchField } = this.state;
+    const { monsters, searchField, title } = this.state;
     const filteredMonsters = monsters.filter(monster =>
-      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
     return (
       <div className="App">
